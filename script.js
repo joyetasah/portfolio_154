82bc7f0b5bbe4a27d54aeb6b64333b4d087f1ea6
Joyeta Saha <joyetasaha287@gmail.com>
2025-04-02 20:36:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+});
